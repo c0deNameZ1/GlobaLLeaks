83360adef9f6049c694d9c0500da8194cb139d9b
c0deNameZ1 <163785454+c0deNameZ1@users.noreply.github.com>
2024-04-06 13:08:26 +0800

    Delete confirmdownload.js

--- confirmdownload.js
+++
@@ -1,6 +0,0 @@
-function confirmDownload(button) {
-    var downloadUrl = button.getAttribute('data-download-url');
-    if (confirm("Do you want to download this file?")) {
-        window.location.href = downloadUrl;
-    }
-}
